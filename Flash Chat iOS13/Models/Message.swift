51fedf975c2da2796d389fc806052c8212c51480
하다현 <dahyun@hadahyeons-MacBook-Pro.local>
2025-08-19 16:28:50 +0900

    컨트롤러 수정 및 Message 모델 생성

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by 하다현 on 8/19/25.
+//  Copyright © 2025 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
